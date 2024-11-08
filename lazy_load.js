e07b6fb36e8409faabd0887700f3c0695c7f8de1
Conrad Ernst <conrad.ernst@drake.edu>
2024-11-08 14:24:48 -0600

    trying to keep the overhead low

---
+++ lazy_load.js
@@ -0,0 +1,23 @@
+function loadIframe(element) {
+    const iframeSrc = element.getAttribute("data-iframe-src");
+    const iframe = document.createElement("iframe");
+    iframe.src = iframeSrc;
+    iframe.width = "100%";
+    iframe.height = "200";
+    iframe.setAttribute("title", element.alt);
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+    element.parentNode.replaceChild(iframe, element);
+}
+
+function loadIframecontainer(element) {
+    const iframeSrc = element.getAttribute("data-iframe-src");
+    const iframe = document.createElement("iframe");
+    iframe.src = iframeSrc;
+    iframe.width = "100%";
+    iframe.height = "200";
+    iframe.setAttribute("title", element.alt);
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+    element.parentNode.replaceChild(iframe, element);
+}
